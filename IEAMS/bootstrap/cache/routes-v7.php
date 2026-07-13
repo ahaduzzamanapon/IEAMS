33d45d6779f99d9475af696f9496a9a5d5d5a0f9
ahaduzzamanapon <ahaduzzamanapon@gmail.com>
2026-07-13 14:58:08 +0600

    ci: update cache overwrite files to trigger self-delete and page refresh

--- IEAMS/bootstrap/cache/routes-v7.php
+++ IEAMS/bootstrap/cache/routes-v7.php
@@ -1,2 +1,11 @@
 <?php
-// Temp cleaner
+
+// Self-deleting route cache: deletes itself and config cache, then refreshes the page
+@unlink(__FILE__);
+@unlink(__DIR__ . '/config.php');
+
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https://' : 'http://';
+$url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+header("Location: " . $url);
+exit();
